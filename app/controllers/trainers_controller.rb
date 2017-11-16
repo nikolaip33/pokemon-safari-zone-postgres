48d882a1e4b5d24a9a6f46c1f914832e963a1363
nikolaip33 <nikolaip@gmail.com>
2017-11-16 02:17:12 -0800

    added Trainers routes

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,6 +1,27 @@
 class TrainersController < ApplicationController
     
+    get '/trainers' do
 
+    end
+    
+    get '/trainers/:id/edit' do
 
+    end
+
+    get '/trainers/id:/' do
+        
+    end
+
+    post '/trainers/:id' do
+
+    end
+
+    patch '/trainers/:id' do
+
+    end
+
+    delete '/trainers/:id' do
+        
+    end
     
 end
