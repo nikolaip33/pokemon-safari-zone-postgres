3e730194d2f419f2098cbf4c2138d24323d50ebb
nikolaip33 <nikolaip@gmail.com>
2017-11-15 20:18:48 -0800

    create PokemonBase from API with all stats

--- app/models/pokemon_base.rb
+++ app/models/pokemon_base.rb
@@ -9,7 +9,13 @@ class PokemonBase < ActiveRecord::Base
         pokemon.height = data["height"]
         pokemon.weight = data["weight"]
         pokemon.type_1 = data["types"][0]["type"]["name"]
-        pokemon.type_2 = data["types"][1]["type"]["name"] if data["types"][1] != nil
+        pokemon.type_2 = data["types"][1]["type"]["name"] if data["types"][1]
+        pokemon.speed = data["stats"][0]["base_stat"]
+        pokemon.special_defense = data["stats"][1]["base_stat"]
+        pokemon.special_attack = data["stats"][2]["base_stat"]
+        pokemon.defense = data["stats"][3]["base_stat"]
+        pokemon.attack = data["stats"][4]["base_stat"]
+        pokemon.hp = data["stats"][5]["base_stat"]
         pokemon.level = 1
         pokemon.save
         pokemon
