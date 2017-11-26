daa0ee24c241e6791db45266b24c6e8ea0075ac6
nikolaip33 <nikolaip@gmail.com>
2017-11-26 05:56:32 -0800

    pokedex changes

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -1,60 +1,44 @@
 class PokedexController < ApplicationController
-
-    get '/pokedex' do
-        redirect "/pokedex/page/1"
-    end
-
-    get '/safari' do
-        @pokemon = PokemonBase.find_or_create_from_api(rand(1..801))
-        erb :"/pokedex/safari"
-    end
-
-    get '/pokedex/:id' do
-        redirect "/pokedex/page/#{params[:id]}"
-    end
-
-    get '/pokedex/pokemon/:id' do
-        if !params[:id].to_i.between?(1,801)
-            flash[:danger] = "Failure! We could not find that Pokédex Entry."
+    
+        get '/pokedex' do
             redirect "/pokedex/page/1"
-        else
-            @pokemon = PokemonBase.find_or_create_from_api(params[:id])
-            erb :"/pokedex/show"
         end
-    end
-
-    post '/pokedex/pokemon/:id' do
-        if logged_in?
-            if current_user.pokeballs > 0
-                current_user.update_attribute(:pokeballs, current_user.pokeballs - 1)
-                @pokemon = Pokemon.create_from_base(params[:id], current_user)
-                flash[:success] = "Success! #{@pokemon.name.capitalize} was successfully caught."
-                redirect "/pokemon/#{@pokemon.id}"
+    
+        get '/safari' do
+            @pokemon = PokemonBase.find_by(id: rand(1..801))
+            erb :"/pokedex/safari"
+        end
+    
+        get '/pokedex/:id' do
+            redirect "/pokedex/page/#{params[:id]}"
+        end
+    
+        get '/pokedex/pokemon/:id' do
+            if !params[:id].to_i.between?(1,801)
+                flash[:danger] = "Failure! We could not find that Pokédex Entry."
+                redirect "/pokedex/page/1"
             else
-                flash[:danger] = "Failure! You do not have any Pokéballs."
-                redirect "/pokedex/pokemon/#{params[:id]}"
+                @pokemon = PokemonBase.find_by(id: params[:id])
+                erb :"/pokedex/show"
             end
-        else
-            "Failure! You are not signed in."
-            redirect "/sign-in"
         end
-    end
-
-    get '/pokedex/page/:page' do
-        if params[:page].to_i.between?(1,40)
-            @pokedex = Pokedex.new(params[:page].to_i)
-            @index = @pokedex.index
-            @active = @pokedex.page
-            @pokelist = @pokedex.validate
-            @navlinks = @pokedex.create_nav
-            erb :"/pokedex/page"
-        elsif params[:page].to_i <= 0
-            redirect "/pokedex/page/1"
-        elsif params[:page].to_i >= 40
-            redirect "/pokedex/page/40"
-        else
-            redirect "/pokedex/page/1"
+    
+        get '/pokedex/page/:page' do
+            if params[:page].to_i.between?(1,40)
+                @pokedex = Pokedex.new(params[:page].to_i)
+                @index = @pokedex.index
+                @active = @pokedex.page
+                @pokelist = @pokedex.pokemon
+                @navlinks = @pokedex.create_nav
+                erb :"/pokedex/page"
+            elsif params[:page].to_i <= 0
+                redirect "/pokedex/page/1"
+            elsif params[:page].to_i >= 40
+                redirect "/pokedex/page/40"
+            else
+                redirect "/pokedex/page/1"
+            end
         end
+    
     end
-
-end
+    
--- app/models/pokedex.rb
+++ app/models/pokedex.rb
@@ -1,80 +1,52 @@
 class Pokedex
-
-    attr_accessor :page, :pokemon
     
-    WHITELIST = [
-        "ho-oh",
-        "jangmo-o",
-        "hakamo-o",
-        "kommo-o",
-        "tapu-koko",
-        "tapu-lele",
-        "tapu-bulu",
-        "tapu-fini",
-        "type-null",
-        "porygon-z",
-        "mime-jr",
-        "mr-mime"
-    ]
-
-    def initialize(page)
-        @page = page.to_i
-        path = "http://pokeapi.co/api/v2/pokemon/#{paginate(self.page.to_i)}"
-        @pokemon = JSON.parse(RestClient.get(path, headers={}))["results"]
-    end
-
-    def validate
-        self.pokemon.collect { |p| WHITELIST.include?(p["name"]) ? p["name"] : p["name"].split("-").first }
-    end
-
-    def create_nav
-        a = [] 
-        first = 1
-        last = 40
-        
-        if self.page <= 5
-          a[0] = first
-          a[1] = 2
-          a[2] = 3
-          a[3] = 4
-          a[4] = 5
-          a[5] = 6
-          a[6] = last-(last)*2/3.ceil
-          a[7] = last-(last)*1/3.ceil
-          a[8] = last
+        attr_accessor :page, :pokemon, :index
     
-        elsif self.page >= last-4
-          a[0] = first
-          a[1] = (self.page)*1/3.ceil
-          a[2] = (self.page)*2/3.ceil
-          a[3] = last-5
-          a[4] = last-4
-          a[5] = last-3
-          a[6] = last-2
-          a[7] = last-1
-          a[8] = last
-        else
-          a[0] = first
-          a[1] = (self.page)*1/3.ceil
-          a[2] = (self.page)*2/3.ceil
-          a[3] = self.page-1
-          a[4] = self.page  
-          a[5] = self.page+1
-          a[6] = last-(last-self.page)*2/3.ceil
-          a[7] = last-(last-self.page)*1/3.ceil
-          a[8] = last
+        def initialize(page)
+            @page = page.to_i
+            @index = ((@page-1)*20)+1
+            range = @page != 40 ? (@index..@index + 19).to_a : (@index..@index + 20).to_a 
+            @pokemon = PokemonBase.find(range)
         end
-        a
+    
+        def create_nav
+            a = [] 
+            first = 1
+            last = 40
+            
+            if self.page <= 5
+              a[0] = first
+              a[1] = 2
+              a[2] = 3
+              a[3] = 4
+              a[4] = 5
+              a[5] = 6
+              a[6] = last-(last)*2/3.ceil
+              a[7] = last-(last)*1/3.ceil
+              a[8] = last
+        
+            elsif self.page >= last-4
+              a[0] = first
+              a[1] = (self.page)*1/3.ceil
+              a[2] = (self.page)*2/3.ceil
+              a[3] = last-5
+              a[4] = last-4
+              a[5] = last-3
+              a[6] = last-2
+              a[7] = last-1
+              a[8] = last
+            else
+              a[0] = first
+              a[1] = (self.page)*1/3.ceil
+              a[2] = (self.page)*2/3.ceil
+              a[3] = self.page-1
+              a[4] = self.page  
+              a[5] = self.page+1
+              a[6] = last-(last-self.page)*2/3.ceil
+              a[7] = last-(last-self.page)*1/3.ceil
+              a[8] = last
+            end
+            a
+        end
+    
     end
-
-    def index
-        ((@page-1)*20)+1
-    end
-
-end
-
-def paginate(n)
-    offset = (n.to_i-1)*20
-    limit = offset == 780 ? 21 : 20
-    "?limit=#{limit}&offset=#{offset}"
-end
